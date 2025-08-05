cfbb0d29ce354b8d19f27d76896d18e96d2baf3c
Rodrigo C. <mistermaster0@gmail.com>
2025-08-05 08:39:11 -0600

    Update fix.py

--- fix_nc_gt/fix.py
+++ fix_nc_gt/fix.py
@@ -14,13 +14,13 @@ from frappe.utils import get_site_name
 @frappe.whitelist()
 def descontar_costo (doc, method):
 	if doc.is_return == True:
-		"custom logic here"
+		        frappe.db.set_value("Sales Invoice",{"name":doc.return_against},{"outstanding_amount":"outstanding_amount" + doc.grand_total})
 	else:
 		pass 
 	
 @frappe.whitelist()
 def contar_costo (doc, method):
 	if doc.is_return == True:
-		"custom logic here"
+		frappe.db.set_value("Sales Invoice",{"name":doc.return_against},{"outstanding_amount":"outstanding_amount" - doc.grand_total})
 	else:
 		pass 
