bd472830e7661e19a6812f1e24779bf842b46d34
Rodrigo C. <mistermaster0@gmail.com>
2025-08-04 22:41:40 -0600

    Update fix.py

--- fix_nc_gt/fix.py
+++ fix_nc_gt/fix.py
@@ -1,7 +1,24 @@
 
+from __future__ import unicode_literals
+
+import json
+from datetime import date, datetime
 
 import frappe
 import requests
 import xmltodict
 from frappe import _
 from frappe.utils import get_site_name
+
+
+@frappe.whitelist()
+def descontar_costo (doc, method):
+	if doc.is_return == True:
+		"custom logic here"
+	else
+	
+@frappe.whitelist()
+def contar_costo (doc, method):
+	if doc.is_return == True:
+		"custom logic here"
+	else
